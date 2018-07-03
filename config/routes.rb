db10cb3ca84b8fb953520357d4af1c9722a739dc
Zac Willmington <zac.willmington@gmail.com>
2018-07-03 15:09:12 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  get 'hello_world', to:
-  'static#hello_world'
+  get 'helloworld', to:
+  'static#helloworld'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
